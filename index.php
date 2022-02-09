32f1700b8cc30a314e91b5638b4e2ae60c4c1fdc
RJVD20 <robinjulian9@gmail.com>
2022-02-09 09:49:01 +0100

    0.1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>  
+    <meta charset="UTF-8">  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>|
+    <div class="welkombox">  
+      <div class="welkomtext">
+        <h1>
+          Welkom op onze site
+        </h1>
+      </div>
+    </div>
+  </body>
+
+</html>
---
+++ php.php
@@ -0,0 +1,11 @@
+<?php
+
+$host= 'locahost' ;
+$user= 'root' ;
+$pass= '';
+$db= 'stempeiling';
+
+$con=mysqli_connect($host,$user,$pass,$db);
+if($con)
+
+?>
