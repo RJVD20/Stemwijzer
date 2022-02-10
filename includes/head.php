68cdfa69b22ba78b627c5f8dcbc531343e74152e
RJVD20 <robinjulian9@gmail.com>
2022-02-10 11:50:52 +0100

    0.1.3

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,39 @@
 <!DOCTYPE html>
-<html>
-<head>  
-    <meta charset="UTF-8">  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>SVS</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="stylesheet.css">
-  </head>
-</html>
+    <style>
+      .headerFont{
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 24px;
+      }
+
+      .subFont{
+        font-family: 'Raleway', sans-serif;
+        font-size: 14px;
+        
+      }
+      
+      .specialHead{
+        font-family: 'Oswald', sans-serif;
+      }
+
+      .normalFont{
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+    </style>
---
+++ vault.php
@@ -0,0 +1,89 @@
+<?php
+include 'includes/head.php';
+?>
+<!DOCTYPE html>
+  </head>
+  <body>
+    <div class="container" style="padding:100px;">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+border-radius: 10px;">
+          
+          <div class="page-header">
+            <br><h2 class="specialHead text-center">Kies hier je partij:</h2>
+          </div>
+          
+          <form action="saveVote.php" method="POST">
+          <div class="form-group">
+            <label>Naam van de stemmer:</label><br>
+            
+            <input type="text" name="voterName" placeholder="Voer hier je volledige naam in" class="form-control"/><br>
+
+            <label>Email van de stemmer:</label><br>
+            <input type="email" name="voterEmail" placeholder="Voer uw mail in" class="form-control"/><br>
+            
+            <h3 class="normalFont text-center">Selecteer hier een partij waarop u wilt stemmen:</h3><br>
+            <div class="radio text-center">
+              
+            
+            <div class="container">
+  <div class="row">
+    <div class="col-sm">
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="JM"> <strong>CDA</strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="JRZ"> <strong>VVD</strong> 
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>PvdA</strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>GroenLinks</strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>Wens4U</strong>
+              </label><br>
+
+    </div>
+    <div class="col-sm">
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="MAD"> <strong>Seniorenpartij </strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="JW"> <strong>JESS Lokaal </strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>SP</strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>D66</strong>
+              </label><br>
+              <label class="partij" for="">
+                <input type="radio" name="selectedCandidate" value="DM"> <strong>PVV</strong>
+              </label><br>
+    </div>
+  </div>
+</div>
+              <br>
+              <hr>
+              <button type="submit" name="submit" class="btn btn-success" style="border-radius:0%">Stem doorgeven</button>
+              <button type="submit" class="btn btn-danger" style="border-radius:0%">Annuleren</button>
+            </div>
+          </div>
+          </form>
+
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
