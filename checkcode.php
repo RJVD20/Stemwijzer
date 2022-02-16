d9dcd215591037aa53004fae1159b86e0e1be0ae
Qualiteit <jordynlxl@gmail.com>
2022-02-16 09:08:10 +0100

    comment edit

--- checkcode.php
+++ checkcode.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if the code exists
+// Check if the code exists and is in the database
 $code = $_POST['code'];
 echo $code;
 
